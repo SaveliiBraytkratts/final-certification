795c4109f8d11f37e7c231888727df48555340d4
unknown <braytkratts@bk.ru>
2023-03-25 21:23:54 +1000

    доработан функционал и создана функция проверки и переноса подходящих строк в другой ммасив

--- certification/Program.cs
+++ certification/Program.cs
@@ -6,7 +6,21 @@
         arrey[i] = Console.ReadLine()!;
     }
 }
+void CheckingStork(string[] arr, string[] ar)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            ar[i] = arr[i];
+            Console.WriteLine(ar[i]);
+        }
+    }
+}
 Console.WriteLine("Введите количество строк:");
 int n = int.Parse(Console.ReadLine()!);
 string[] stringArray = new string[n];
+string[] stringArray2 = new string[n];
 CreationArray(stringArray);
+Console.WriteLine("массив из сторк, длина котрых меньше или ровна 3 символа");
+CheckingStork(stringArray, stringArray2);
