eadea7904a460b6cea16ff4cd8a70e305aa2f8b1
unknown <braytkratts@bk.ru>
2023-03-25 20:23:09 +1000

    создание функции для заполнения массива

---
+++ certification/Program.cs
@@ -0,0 +1,10 @@
+﻿void CreationArray(string[] arrey)
+{
+    Console.WriteLine("Введите строки :");
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        arrey[i] = Console.ReadLine()!;
+    }
+}
+string[] stringArray = new string[4];
+CreationArray(stringArray);
